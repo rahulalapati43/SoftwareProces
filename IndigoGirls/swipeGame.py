fe56879d7585b9e4b260c26d6c311e625fd2c917
rahul.alapati43 <rza0037@tigermail.auburn.edu>
2017-10-16 13:23:56 -0500

    Green Light for invalid direction

--- IndigoGirls/swipeGame.py
+++ IndigoGirls/swipeGame.py
@@ -1,2 +1,10 @@
 def swipeGame(messageDictionary):
-    pass
+    outputDictionary = {}
+    if "direction" not in messageDictionary.keys():
+        outputDictionary["gameStatus"] = "error: missing direction"
+
+    if (messageDictionary["direction"] != "up" or messageDictionary["direction"] != "down" or messageDictionary[
+        "direction"] != "left" or messageDictionary["direction"] != "right"):
+        outputDictionary["gameStatus"] = "error: invalid direction"
+
+    return outputDictionary

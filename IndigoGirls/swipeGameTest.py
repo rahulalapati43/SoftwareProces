a2157b6956ac5811a93342e057024553d20c30ed
rahul.alapati43 <rza0037@tigermail.auburn.edu>
2017-10-16 14:05:46 -0500

    Red Light for rowCount

--- IndigoGirls/swipeGameTest.py
+++ IndigoGirls/swipeGameTest.py
@@ -11,10 +11,22 @@ class SwipeGameTest(TestCase):
     #     outputDictionary = swipeGame(inputDictionary)
     #     self.assertEquals(resultDictionary,outputDictionary)
 
-     def test100_020_shouldValidatemissingdirection(self):
-         inputDictionary = {}
-         inputDictionary["op"] = "swipe"
-         resultDictionary = {}
-         resultDictionary["gameStatus"] = "error: missing direction"
-         outputDictionary = swipeGame(inputDictionary)
-         self.assertEquals(resultDictionary,outputDictionary)
+     # def test100_020_shouldValidatemissingdirection(self):
+     #     inputDictionary = {}
+     #     inputDictionary["op"] = "swipe"
+     #     resultDictionary = {}
+     #     resultDictionary["gameStatus"] = "error: missing direction"
+     #     outputDictionary = swipeGame(inputDictionary)
+     #     self.assertEquals(resultDictionary,outputDictionary)
+
+       def test100_030_shouldValidaterowCount(self):
+           inputDictionary = {}
+           inputDictionary["op"] = "swipe"
+           inputDictionary["direction"] = "left"
+           boardDictionary = {}
+           boardDictionary["rowCount"] = 0
+           inputDictionary["board"] = boardDictionary
+           resultDictionary = {}
+           resultDictionary["gameStatus"] = "error: invalid row count"
+           outputDictionary = swipeGame(inputDictionary)
+           self.assertEquals(resultDictionary,outputDictionary)

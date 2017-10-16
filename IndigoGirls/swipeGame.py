7cbb0bca3899100d38546767e9fd4a8b17a70992
rahul.alapati43 <rza0037@tigermail.auburn.edu>
2017-10-16 14:07:25 -0500

    Red Light for wrong reason, corrected code

--- IndigoGirls/swipeGame.py
+++ IndigoGirls/swipeGame.py
@@ -3,7 +3,7 @@ def swipeGame(messageDictionary):
     if "direction" not in messageDictionary.keys():
         outputDictionary["gameStatus"] = "error: missing direction"
 
-    elif (messageDictionary["direction"] != "up" or messageDictionary["direction"] != "down" or messageDictionary["direction"] != "left" or messageDictionary["direction"] != "right"):
+    elif (messageDictionary["direction"] != "up" and messageDictionary["direction"] != "down" and messageDictionary["direction"] != "left" and messageDictionary["direction"] != "right"):
         outputDictionary["gameStatus"] = "error: invalid direction"
 
     return outputDictionary

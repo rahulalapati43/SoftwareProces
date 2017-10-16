64edcafdcea2c133f15ba55f23dc436a0ee4b93b
rahul.alapati43 <rza0037@tigermail.auburn.edu>
2017-10-16 14:18:39 -0500

    Green Light for rowCount

--- IndigoGirls/swipeGameTest.py
+++ IndigoGirls/swipeGameTest.py
@@ -19,14 +19,26 @@ class SwipeGameTest(TestCase):
      #     outputDictionary = swipeGame(inputDictionary)
      #     self.assertEquals(resultDictionary,outputDictionary)
 
-       def test100_030_shouldValidaterowCount(self):
+       # def test100_030_shouldValidaterowCount(self):
+       #     inputDictionary = {}
+       #     inputDictionary["op"] = "swipe"
+       #     inputDictionary["direction"] = "left"
+       #     boardDictionary = {}
+       #     boardDictionary["rowCount"] = 0
+       #     inputDictionary["board"] = boardDictionary
+       #     resultDictionary = {}
+       #     resultDictionary["gameStatus"] = "error: rowCount is out of bounds"
+       #     outputDictionary = swipeGame(inputDictionary)
+       #     self.assertEquals(resultDictionary,outputDictionary)
+
+       def test100_040_shouldValidaterowCountType(self):
            inputDictionary = {}
            inputDictionary["op"] = "swipe"
            inputDictionary["direction"] = "left"
            boardDictionary = {}
-           boardDictionary["rowCount"] = 0
+           boardDictionary["rowCount"] = " "
            inputDictionary["board"] = boardDictionary
            resultDictionary = {}
-           resultDictionary["gameStatus"] = "error: rowCount is out of bounds"
+           resultDictionary["gameStatus"] = "error: rowCount is not an integer"
            outputDictionary = swipeGame(inputDictionary)
            self.assertEquals(resultDictionary,outputDictionary)

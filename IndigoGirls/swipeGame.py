96b91e219257ae2b930982d74c32baa19e8719c1
rahul.alapati43 <rza0037@tigermail.auburn.edu>
2017-10-16 22:15:01 -0500

    Green Light for swipe left

--- IndigoGirls/swipeGame.py
+++ IndigoGirls/swipeGame.py
@@ -56,7 +56,53 @@ def swipeGame(messageDictionary):
                             outputDictionary["gameStatus"] = "error: No fewer than two items can be GT 0"
 
     if (outputDictionary["gameStatus"] == "underway"):
+
+        x = len(messageDictionary["board"]["grid"])
+
+        sumElements = 0
+
+        boardDictionary = {}
+
+        outList = []
+        for y in range(0, x):
+            outList.append(0)
+
         if (messageDictionary["direction"] == "left"):
-            pass
+
+            inListIndex = outListIndex = 0
+            previousElement = 0
+
+            for row in range(0, messageDictionary["board"]["rowCount"]):
+                outListIndex = inListIndex
+                previousElement = 0
+                for column in range(0, messageDictionary["board"]["columnCount"]):
+                    if (messageDictionary["board"]["grid"][inListIndex] != 0):
+                        if (previousElement == 0):
+                            previousElement = messageDictionary["board"]["grid"][inListIndex]
+                        else:
+                            if (previousElement == messageDictionary["board"]["grid"][inListIndex]):
+                                power = messageDictionary["board"]["grid"][inListIndex]
+                                sumElements = 2 * (2 ** power)
+                                outList[outListIndex] = math.log(sumElements, 2)
+                                outListIndex = outListIndex + 1
+                                previousElement = 0
+                            else:
+                                outList[outListIndex] = previousElement
+                                outListIndex = outListIndex + 1
+                                previousElement = 0
+                    inListIndex = inListIndex + 1
+                if previousElement != 0:
+                    outList[outListIndex] = previousElement
+
+            compareResult = cmp(messageDictionary["board"]["grid"], outList)
+
+            if (compareResult != 0):
+                outputDictionary["score"] = sumElements
+                boardDictionary["columnCount"] = messageDictionary["board"]["columnCount"]
+                boardDictionary["rowCount"] = messageDictionary["board"]["rowCount"]
+                boardDictionary["grid"] = outList
+                outputDictionary["board"] = boardDictionary
+            elif (compareResult == 0):
+                outputDictionary["gameStatus"] = "error: no tiles can be shifted"
 
     return outputDictionary

2223f581d3f0b466482402deb05cd14265c63abb
rahul.alapati43 <rza0037@tigermail.auburn.edu>
2017-11-07 17:35:18 -0600

    Green Light for validating moves is an integer

--- IndigoGirls/recommendGame.py
+++ IndigoGirls/recommendGame.py
@@ -6,5 +6,7 @@ def recommendGame(messageDictionary):
     if "moves" not in messageDictionary.keys():
         messageDictionary["moves"] = 0
 
+    elif (isinstance(messageDictionary["moves"], int) == False):
+        outputDictionary["gameStatus"] = "error: moves is not an integer"
 
     return outputDictionary

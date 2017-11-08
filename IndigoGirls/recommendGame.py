261a055612caf4193cef14ba58185dcb5f0a9040
rahul.alapati43 <rza0037@tigermail.auburn.edu>
2017-11-07 18:00:31 -0600

    Green Light for columnCount type must be an integer

--- IndigoGirls/recommendGame.py
+++ IndigoGirls/recommendGame.py
@@ -18,4 +18,7 @@ def recommendGame(messageDictionary):
     elif "columnCount" not in messageDictionary["board"]:
         outputDictionary["gameStatus"] = "error: missing columnCount"
 
+    elif (isinstance(messageDictionary["board"]["columnCount"], int) == False):
+        outputDictionary["gameStatus"] = "error: columnCount is not an integer"
+
     return outputDictionary

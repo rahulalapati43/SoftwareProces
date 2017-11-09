3f6271c576328880ff4f3fde354b150270642dc3
rahul.alapati43 <rza0037@tigermail.auburn.edu>
2017-11-09 06:40:34 -0600

    Green light for recommend invalid moves

--- IndigoGirls/test/recommendCheck.py
+++ IndigoGirls/test/recommendCheck.py
@@ -19,3 +19,7 @@ print(("Input string:\t{0}\nOutput string:\t{1}\n").format(validJson, validResul
 validJson = '{"op": "recommend", "moves": 1, "board": {"columnCount": 4, "rowCount": 4, "grid": [1,2,3,4,4,3,2,1,1,2,3,4,4,3,2,1]}}'
 validResult = dispatch.dispatch(validJson)
 print(("Input string:\t{0}\nOutput string:\t{1}\n").format(validJson, validResult))
+
+validJson = '{"op": "recommend", "moves": "0", "board": {"columnCount": 4, "rowCount": 4, "grid": [0,0,0,1,0,0,0,1,2,2,0,0,1,0,2,2]}}'
+validResult = dispatch.dispatch(validJson)
+print(("Input string:\t{0}\nOutput string:\t{1}\n").format(validJson, validResult))

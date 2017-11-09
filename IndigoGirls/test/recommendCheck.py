9c8ee68dff23c34d576793eecc67b26d34f9c7f6
rahul.alapati43 <rza0037@tigermail.auburn.edu>
2017-11-09 06:32:03 -0600

    Green light for recommend 0 moves

--- IndigoGirls/test/recommendCheck.py
+++ IndigoGirls/test/recommendCheck.py
@@ -4,3 +4,8 @@ validJson = '{"op": "recommend", "board": {"columnCount": 4, "rowCount": 4, "gri
 validResult = dispatch.dispatch(validJson)
 print(("Input string:\t{0}\nOutput string:\t{1}\n").format(validJson, validResult))
 
+validJson = '{"op": "recommend", "moves": 0, "board": {"columnCount": 4, "rowCount": 4, "grid": [0,0,0,1,0,0,0,1,2,2,0,0,1,0,2,2]}}'
+validResult = dispatch.dispatch(validJson)
+print(("Input string:\t{0}\nOutput string:\t{1}\n").format(validJson, validResult))
+
+

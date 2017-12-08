099ec69905f0ec4e342d9c4ef16f72927583f7b5
rahul.alapati43 <rza0037@tigermail.auburn.edu>
2017-12-08 17:54:17 -0600

    Green Light for column Count LE100

--- IndigoGirls/predictGame.py
+++ IndigoGirls/predictGame.py
@@ -26,8 +26,9 @@ def predictGame(messageDictionary):
     elif (isinstance(messageDictionary["board"]["columnCount"],int) == False):
         outputDictionary["gameStatus"] = "error: columnCount is not an integer"
 
-    elif (messageDictionary["board"]["columnCount"] <= 1):
+    elif ((messageDictionary["board"]["columnCount"] <= 1) or (messageDictionary["board"]["columnCount"] > 100)):
         outputDictionary["gameStatus"] = "error: columnCount is out of bounds"
+
 
     return outputDictionary
 

10e335763635121b7af0138817952e712e95858e
rahul.alapati43 <rza0037@tigermail.auburn.edu>
2017-12-08 17:10:10 -0600

    Green Light for invalid direction

--- IndigoGirls/predictGame.py
+++ IndigoGirls/predictGame.py
@@ -4,6 +4,10 @@ def predictGame(messageDictionary):
     if "direction" not in messageDictionary.keys():
         outputDictionary["gameStatus"] = "error: missing direction"
 
+    elif (messageDictionary["direction"].lower() != "up" and messageDictionary["direction"].lower() != "down" and
+                  messageDictionary["direction"].lower() != "left" and messageDictionary["direction"].lower() != "right"):
+        outputDictionary["gameStatus"] = "error: invalid direction"
+
     return outputDictionary
 
 
